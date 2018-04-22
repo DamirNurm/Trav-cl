b0d212bd02a73cd72c09e3f5183a8233a61bf850
Morfg <Morfg>
2018-04-22 18:07:14 +0500

    изм.тест

--- project-name/src/test/java/com/sanifrey/test1/Formula2Test.java
+++ project-name/src/test/java/com/sanifrey/test1/Formula2Test.java
@@ -1,27 +1,45 @@
+/** 
+* Устанавливаем принадлежность класса к пакету ProjectTest 
+*/ 
 package com.sanifrey.test1;
-
-import org.junit.Before;
+/** 
+* Импортируем библиотеку 
+*/ 
+import static org.junit.Assert.*;
+/** 
+* Импортируем библиотеку 
+*/ 
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-
+/** 
+* Основной класс 
+*/ 
 public class Formula2Test {
-
-	private Formula formula;
-
-    @Before
-    public void setup() {
-    	formula = new Formula(null);
-    }
-
-    @Test
-    public void testGetMessage() {
-    	assertEquals("39.0",formula.PFormula("60", "2.5", "8.4"));
-    }
-
-    @Test
-    public void testGetMessage2() {
-    	assertEquals("49.0",formula.PFormula("70", "3", "7"));
-    }
-
+/** 
+* Объявляем переменную типа Formula 
+*/ 
+private Formula formula;
+/** 
+* Обозначаем , что следующий метод будет тестируемым 
+*/ 
+@Test
+/** 
+* тестируемый метод 
+*/ 
+public void test() {
+/** 
+* Инициализируем переменную formula 
+*/ 
+ formula = new Formula(null);
+ /** 
+ * Тестируем метод с входными параметрами 60, 2.5, 8.4 и с предполагаемым результатом 39.0 для проверки работы программы с дробными числами.
+ * Тестируемый метод выполняет расчёт свободной площади в комнате по формуле: Z=S-a*b.
+ * Z - Значение свободной площади в комнате;
+ * S - Значение всей площади в комнате, передается первым при вызове метода;
+ * a - Значение ширины объекта, передается вторым при вызове метода;
+ * b - Значение длины объекта, передается третьим при вызове метода;
+ * Предполагается, что программа выполнит следующие расчёты: 60-2.5*8.4 и
+ * вернёт значение: 39.0.
+ */ 
+assertEquals("39.0",formula.PFormula("60", "2.5", "8.4"));
+}
 }
