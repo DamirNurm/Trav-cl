ab955066df2eb177a43d3d5505fa009e50de311c
Morfg <Morfg>
2018-04-29 16:11:06 +0500

    Вроде код

--- project-name/src/main/java/com/sanifrey/test1/Otdel.java
+++ project-name/src/main/java/com/sanifrey/test1/Otdel.java
@@ -9,9 +9,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
-import javax.swing.LayoutStyle.ComponentPlacement;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
 /**
  * Подключаем класс событий
  */
@@ -30,8 +27,79 @@ public class Otdel {
 	 * textField - Для отображения значения всей площади комнаты.
 	 * textField_1 - Для отображения значения свободного места в комнате.
 	 */
-	private JTextField textField;
-	private JTextField textField_1;
+	
+	/**
+	 * Объявляем кнопки.
+	 * button_save - Кнопка для сохранения результатов.
+	 * button_Menu - Кнопка сокрытия окна "Отделочник".
+	 */
+	private JButton button_save;
+	private JButton button_Menu;
+	/**
+	 * Объявляем поля ввода JTextField. 
+	 * textField_AllArea - Поле для вывода значения всей площади комнаты. 
+	 * textField_FreeArea - Поле для вывода значения свободной площади.
+	 */
+	private JTextField textField_AllArea;
+	private JTextField textField_FreeArea;
+	/**
+	 * Объявляем элементы для отображения текста. 
+	 * label_AllArea - Отображает "Введите площадь всей комнаты". 
+	 * label_ParametersObj - Отображает "Параметры объекта". 
+	 * label_WidthObj - Отображает "Ширина". 
+	 * label_LengthObj - Отображает "Длина". 
+	 * label_AmountObj - Отображает "Количество объектов".
+	 */
+	private JLabel label_AllArea;
+	private JLabel label_FreeArea;
+	
+	private ComponentsCreator cc;
+	private PanelCreator pc;
+	/**
+	 * Объявляем массив ButtonName для хранения идентификаторов JButton.
+	 */
+	private JButton ButtonName[]= {button_save,button_Menu};
+	/**
+	 * Объявляем массив Button_keys_label для хранения названия кнопок.
+	 */
+	private String Button_keys_label[] = {"Сохранить","В меню"};
+	/**
+	 * Объявляем массив Button_Bounds для хранения координат расположения кнопки и её размеров.
+	 */
+	private int Button_Bounds[] = {10, 180, 105, 23, 340, 227, 84, 23};
+	
+	/**
+	 * Объявляем массив ButtonName для хранения идентификаторов JTextField.
+	 */
+	private JTextField TextFieldName[]= {textField_AllArea,textField_FreeArea};
+	/**
+	 * Объявляем массив TextField_Bounds для хранения координат расположения поля ввода и её размеров.
+	 */
+	private int TextField_Bounds[] = {10, 31, 86, 20,10, 77, 86, 20};
+	/**
+	 * Объявляем массив TextField_Editable для хранения информации о возможности редактировать конкретное поле.
+	 */
+	private boolean TextField_Editable[] = {false,false};
+	/**
+	 * Объявляем массив ButtonName для хранения идентификаторов JLabel.
+	 */
+	private JLabel JLabelName[] = {label_AllArea, label_FreeArea};
+	/**
+	 * Объявляем массив JLabel_Bounds для хранения координат расположения label'a и её размеров.
+	 */
+	private int JLabel_Bounds[] = {10, 11, 137, 14,10, 57, 137, 14};
+	/**
+	 * Объявляем массив JLabel_text для хранения информации, что будет написано в label'e.
+	 */
+	private String JLabel_text[] = {"Общая площадь","Свободная площадь"};
+	/**
+	 * Создаем экземпляр класса eHandler и возвращает ссылку на вновь созданный объект
+	 */
+	private eHandler handler = new eHandler();
+	/**
+	 * Счётчик используется для подсчёта сдвига в массивах, связанных с координатами и размеров компонента.
+	 */
+	private int Counter;
 	/**
 	 * Вызываем конструктор
 	 */
@@ -50,87 +118,128 @@ public class Otdel {
 		 */
 		frame = new JFrame();
 		/**
-		 * Отображаем окно
+		 * Создаем экземпляр класса ComponentsCreator
 		 */
-		frame.setVisible(arg);
+		cc = new ComponentsCreator(frame);
 		/**
-		 * Устанавливаем название окна
+		 * Создаем экземпляр класса PanelCreator
 		 */
-		frame.setTitle("Отделочник");
+		pc = new PanelCreator();
 		/**
-		 * Указываем координаты верхней левой вершины окна, а также его ширину и высоту.
+		 * Создаём простые компоненты класса JButton
 		 */
-		frame.setBounds(100, 100, 450, 300);
-		/**
-		 * Указываем операцию, которая будет произведена при закрытии окна.
+        for (int i=0;i<=1;i++) {
+        	ButtonName[i] = new JButton();
+    		/**
+    		 * При помощи метода PJButtonSettings задаем параметры для кнопок.
+    		 * 1-ый параметр - Для какого button'а задаются следующие параметры.
+    		 * 2-ой параметр - Что будет написано в этой кнопке.
+    		 * 3-ий параметр - Координата размещения по оси X.
+    		 * 4-ый параметр - Координата размещения по оси Y.
+    		 * 5-ый параметр - Ширина.
+    		 * 6-ой параметр - Длина.
+    		 */
+            cc.PJButtonSettings(ButtonName[i],Button_keys_label[i], Button_Bounds[Counter+i], Button_Bounds[Counter+i+1], Button_Bounds[Counter+i+2], Button_Bounds[Counter+i+3]);
+            ButtonName[i].addActionListener(handler);
+            Counter+=3;
+        }
+  		/**
+		 * Обнуление счётчика для использования в следующем массиве
 		 */
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		/**
-		 * Создаём простые компоненты класса JLabel
-		 */
-		JLabel label = new JLabel("Общая площадь");
-		label.setBounds(10, 11, 84, 14);
-		JLabel label_1 = new JLabel("Свободная площадь");
-		label_1.setBounds(10, 57, 105, 14);
+        Counter=0;
 		/**
 		 * Создаём простые компоненты класса JTextField
 		 */
-		textField_1 = new JTextField();
-		textField_1.setBounds(10, 77, 86, 20);
-		textField = new JTextField();
-		textField.setBounds(10, 31, 86, 20);
-		/**
-		 * Указываем количество символов в строке
+        for (int i=0;i<=1;i++) {
+    		/**
+    		 * Создаем экземпляр класса JTextField
+    		 */
+        	TextFieldName[i] = new JTextField();
+    		/**
+    		 * При помощи метода PJTextFieldSettings задаем параметры для textField-ов.
+    		 * 1-ый параметр - Для какого textField'а задаются следующие параметры.
+    		 * 2-ой параметр - Разрешено ли редактировать.
+    		 * 3-ий параметр - Координата размещения по оси X.
+    		 * 4-ый параметр - Координата размещения по оси Y.
+    		 * 5-ый параметр - Ширина.
+    		 * 6-ой параметр - Длина.
+    		 */
+            cc.PJTextFieldSettings(TextFieldName[i],TextField_Editable[i], TextField_Bounds[Counter+i], TextField_Bounds[Counter+i+1], TextField_Bounds[Counter+i+2], TextField_Bounds[Counter+i+3]);
+    		/**
+    		 * К "Counter" прибавляем "3" для того, чтобы правильно получать данные из массива TextField_Bounds с информацией о координатах и размеров полей ввода.
+    		 */
+            Counter+=3;
+        }
+  		/**
+		 * Обнуление счётчика для использования в следующем массиве
 		 */
-		textField_1.setColumns(10);
-		textField.setColumns(10);
+        Counter=0;
 		/**
-		 * Создаём простой компонент button класса JButton
+		 * Создаём простые компоненты класса JLabel
 		 */
-		JButton button = new JButton("Сохранить");
-		button.setBounds(10, 180, 87, 23);
+        for (int i=0;i<=1;i++) {
+        	JLabelName[i] = new JLabel();
+    		/**
+    		 * При помощи метода PJLabelSettings задаем параметры для label-ов.
+    		 * 1-ый параметр - Для какого label'а задаются следующие параметры.
+    		 * 2-ой параметр - Что будет показано этим label'ом.
+    		 * 3-ий параметр - Координата размещения по оси X.
+    		 * 4-ый параметр - Координата размещения по оси Y.
+    		 * 5-ый параметр - Ширина.
+    		 * 6-ой параметр - Длина.
+    		 */
+            cc.PJLabelSettings(JLabelName[i],JLabel_text[i], JLabel_Bounds[Counter+i], JLabel_Bounds[Counter+i+1], JLabel_Bounds[Counter+i+2], JLabel_Bounds[Counter+i+3]);
+            Counter+=3;
+        }
 		/**
-		 * Добавляем слушателя к кнопке button с помощью вызова addActionListener
+		 * При помощи метода PCreatePanel задаем необходимые параметры для frame
 		 */
-		button.addActionListener(new ActionListener() {
+		pc.PCreatePanel(frame, false, "Отделочник");
+		
+		
+	}
+	
+	private class eHandler implements ActionListener {
+		
+		/**
+		 * Интерфейс ActionListener требует только реализации одного метода —
+		 * actionPerformed.
+		 * 
+		 * ActionEvent - событие
+		 */
+		public void actionPerformed(ActionEvent e) {
 			/**
-			 * Интерфейс ActionListener требует только реализации одного метода —
-			 * actionPerformed.
+			 * Проверка нажатия на кнопку button_save
 			 */
-			public void actionPerformed(ActionEvent e) {
-				/**
-				 * Вызываем метод для сохранения данных в файл
-				 */
+			if (e.getSource() == getButtonName(0)) {
 				SaveInFile();
 			}
-		});
-		/**
-		 * Создаём простой компонент button_1 класса JButton
-		 */
-		JButton button_1 = new JButton("В меню");
-		button_1.setBounds(355, 227, 69, 23);
-		/**
-		 * Добавляем слушателя к кнопке button_1 с помощью вызова addActionListener
-		 */
-		button_1.addActionListener(new ActionListener() {
 			/**
-			 * Интерфейс ActionListener требует только реализации одного метода —
-			 * actionPerformed.
+			 * Проверка нажатия на кнопку button_Menu
 			 */
-			public void actionPerformed(ActionEvent e) {
+			else if(e.getSource() == getButtonName(1)){
 				/**
-				 * Скрываем окно "Отделочник"
+				 * Скрываем окно "Планировщик"
 				 */
 				frame.setVisible(false);
 			}
-		});
-		frame.getContentPane().setLayout(null);
-		frame.getContentPane().add(label);
-		frame.getContentPane().add(textField);
-		frame.getContentPane().add(label_1);
-		frame.getContentPane().add(textField_1);
-		frame.getContentPane().add(button);
-		frame.getContentPane().add(button_1);
+		}
+
+	}
+	/**
+	 * Метод для обновления текстовых полей при открытии окна "Отделочник"
+	 */
+	private void UpdateTextFields() {
+		/**
+		 * Заполняем поле textField_AllArea в окне "Отделочник" получая значения через геттер
+		 * getAllArea()
+		 */
+		setTextField(0,String.valueOf(Formula.getAllArea()));
+		/**
+		 * Заполняем поле textField_FreeArea в окне "Отделочник" получая значения через геттер
+		 * getFreeArea()
+		 */
+		setTextField(1, String.valueOf(Formula.getFreeArea()));
 	}
 	/**
 	 * Метод для сохранения в файл
@@ -145,32 +254,29 @@ public class Otdel {
 		 */
 		sf.PSaveInFile();
 	}
-	/**
-	 * Метод для обновления текстовых полей при открытии окна "Отделочник"
-	 */
-	private void UpdateTextFields() {
+	public JButton getButtonName(int i) {
 		/**
-		 * Заполняем поле textField в окне "Отделочник" получая значения через геттер
-		 * getAllArea()
+		 * Получаем значение из поля ввода TextFieldName[i]
 		 */
-		textField.setText(String.valueOf(Formula.getAllArea()));
+		return ButtonName[i];
+	}
+	public void setTextField(int i, String text) {
 		/**
-		 * Заполняем поле textField_1 в окне "Отделочник" получая значения через геттер
-		 * getFreeArea()
+		 * Записываем в TextFieldName[i] значение text
 		 */
-		textField_1.setText(String.valueOf(Formula.getFreeArea()));
+		TextFieldName[i].setText(text);
 	}
 	/**
 	 * Публичный метод для установки в окне "Планировщик" значения setVisible
 	 */
 	public void Visiable(boolean arg) {
 		frame.setVisible(arg);
+		/**
+		 * Обновляем поля ввода
+		 */
 		UpdateTextFields();
 	}
 	/**
 	 * Публичный метод для инициализации окна "Планировщик"
 	 */
-	public void init(boolean arg) {
-		initialize(arg);
-	}
 }
