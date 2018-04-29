cf2f2e6867cc77a1a7577368b7cbd2e1a1783bc9
Morfg <Morfg>
2018-04-29 12:37:28 +0500

    мелкое изменение

--- project-name/src/main/java/com/sanifrey/test1/SaveFile.java
+++ project-name/src/main/java/com/sanifrey/test1/SaveFile.java
@@ -42,8 +42,8 @@ public class SaveFile {
 			writer.println("=======================\n" + "\nTOTAL AREA: " + Formula.getAllArea() + "\nFREE AREA: "
 					+ Formula.getFreeArea() + "\nObjects: "+ Formula.getAmount());
 			for (i = 1; i < Formula.getAmount() + 1; i++) {
-				writer.println("=======================\n" + "\nObject №" + i + "\nwidth:" + obj[i].getWidth()
-						+ "\nlength:" + obj[i].getLength() + "\nOccupies Area:" + obj[i].getArea());
+				writer.println("=======================\n" + "\nObject №" + i + ";\nwidth:" + obj[i].getWidth()
+						+ ";\nlength:" + obj[i].getLength() + ";\nOccupies Area:" + obj[i].getArea());
 			}
 
 			/**
