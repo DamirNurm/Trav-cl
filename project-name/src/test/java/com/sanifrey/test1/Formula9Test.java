c8617b5a08e2a3f7b97d5dd8f0b114c3e1d1ec36
user <yo.98@mail.ru>
2018-04-29 22:56:30 +0500

    доб.тест

--- project-name/src/test/java/com/sanifrey/test1/Formula9Test.java
+++ project-name/src/test/java/com/sanifrey/test1/Formula9Test.java
@@ -40,7 +40,7 @@ public void test() {
  * Предполагается, что программа выполнит следующие расчёты: 60-3*4 и
  * вернёт значение: 48.
  */  formula.PublicAddObject( "3", "4");
-assertEquals("48",formula.PFormula("60"));
+assertEquals("48.0",formula.PFormula("60"));
 formula.PublicDeleteObjects();
 }
 }
